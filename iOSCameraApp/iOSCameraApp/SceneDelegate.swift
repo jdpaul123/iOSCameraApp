f6e5ea0fdfafd74434e40c456a83d7c8a92b69de
JD Paul <jdpaul123@gmail.com>
2023-11-12 09:41:36 -0800

    Created iOS Camera App and added a button for capturing photos

---
+++ iOSCameraApp/iOSCameraApp/SceneDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.swift
+//  iOSCameraApp
+//
+//  Created by Jonathan Paul on 11/12/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ iOSCameraApp/iOSCameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSCameraApp
+//
+//  Created by Jonathan Paul on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
