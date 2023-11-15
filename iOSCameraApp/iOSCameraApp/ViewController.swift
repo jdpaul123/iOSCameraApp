a07e46223a960e5c64017fed030edf421043f7f8
JD Paul <jdpaul123@gmail.com>
2023-11-14 20:30:13 -0800

    Added KVO handling app interuptions. Added a 'resume' button and a 'camera unavailable' label to help deal with interuptions

--- iOSCameraApp/iOSCameraApp/ViewController.swift
+++ iOSCameraApp/iOSCameraApp/ViewController.swift
@@ -58,6 +58,10 @@ class ViewController: UIViewController, AVCapturePhotoOutputReadinessCoordinator
     // Create the PreviewView which is a view that displays the AVCaptureVideoPreviewLayer as it's backing layer.
     @IBOutlet private weak var previewView: PreviewView!
 
+    // MARK: Labels and Buttons
+    @IBOutlet var cameraUnavailableLabel: UILabel!
+    @IBOutlet var resumeButton: UIButton!
+
     // MARK: Capturing Photos Properties
     @IBOutlet private weak var cameraCaptureButton: UIButton!
 
@@ -240,7 +244,7 @@ class ViewController: UIViewController, AVCapturePhotoOutputReadinessCoordinator
             // Create our input device that will be connected to the session
             let videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
 
-            // TODO: Come back to KVO for the app
+            // TODO: Come back to this when implimenting switching cameras
 //            AVCaptureDevice.self.addObserver(self, forKeyPath: "systemPreferredCamera", options: [.new], context: &systemPreferredCameraContext)
 
             // Finally, set the sessions input video device
@@ -332,6 +336,32 @@ class ViewController: UIViewController, AVCapturePhotoOutputReadinessCoordinator
         let photoSettings = self.setUpPhotoSettings()
         DispatchQueue.main.async {
             self.photoSettings = photoSettings
+        }
+    }
+
+    @IBAction private func resumeInterruptedSession(_ resumeButton: UIButton) {
+        sessionQueue.async {
+            // The session might fail to start running, for example, if a phone
+            // or FaceTime call is still using audio or video. This failure is
+            // communicated by the session posting a runtime error notification.
+            // To avoid repeatedly failing to start the session, only try to
+            // restart the session in the error handler if you aren't trying to
+            // resume the session.
+            self.session.startRunning()
+            self.isSessionRunning = self.session.isRunning
+            if !self.session.isRunning {
+                DispatchQueue.main.async {
+                    let message = NSLocalizedString("Unable to resume", comment: "Alert message when unable to resume the session running")
+                    let alertController = UIAlertController(title: "AVCam", message: message, preferredStyle: .alert)
+                    let cancelAction = UIAlertAction(title: NSLocalizedString("OK", comment: "Alert OK button"), style: .cancel, handler: nil)
+                    alertController.addAction(cancelAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.resumeButton.isHidden = true
+                }
+            }
         }
     }
 
@@ -452,6 +482,35 @@ class ViewController: UIViewController, AVCapturePhotoOutputReadinessCoordinator
         return livePhotoMovieURL
     }
 
+    private func focus(with focusMode: AVCaptureDevice.FocusMode,
+                       exposureMode: AVCaptureDevice.ExposureMode,
+                       at devicePoint: CGPoint,
+                       monitorSubjectAreaChange: Bool) {
+        sessionQueue.async {
+            let device = self.videoDeviceInput.device
+            do {
+                try device.lockForConfiguration()
+
+                // Setting (focus/exposure)PointOfInterest alone does not initiate a (focus/exposure) operation.
+                // Call set(Focus/Exposure)Mode() to apply the new point of interest.
+                if device.isFocusPointOfInterestSupported && device.isFocusModeSupported(focusMode) {
+                    device.focusPointOfInterest = devicePoint
+                    device.focusMode = focusMode
+                }
+
+                if device.isExposurePointOfInterestSupported && device.isExposureModeSupported(exposureMode) {
+                    device.exposurePointOfInterest = devicePoint
+                    device.exposureMode = exposureMode
+                }
+
+                device.isSubjectAreaChangeMonitoringEnabled = monitorSubjectAreaChange
+                device.unlockForConfiguration()
+            } catch {
+                print("Could not lock device for configuration: \(error)")
+            }
+        }
+    }
+
     // MARK: Device Configuration
 
 
@@ -500,8 +559,151 @@ class ViewController: UIViewController, AVCapturePhotoOutputReadinessCoordinator
         }
         keyValueObservations.append(keyValueObservation)
 
-        // TODO: Observe app interuptions and deal with them
+        // Observe interuptions
+        // TODO: I think this is for changing the focus as the camera's focus or exposure changes to focus on the middle of the screen?
+        NotificationCenter.default.addObserver(self, selector: #selector(subjectAreaDidChange),
+                                               name: .AVCaptureDeviceSubjectAreaDidChange,
+                                               object: videoDeviceInput.device)
+
+        // If there is a runtime error, this is how we deal with it
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(sessionRuntimeError),
+                                               name: .AVCaptureSessionRuntimeError,
+                                               object: session)
+
+        // A session can only run when the app is fill screen. It will be
+        // interrupted in a multi-app layout, introduced in iOS 9, see also the
+        // documentation of AVCaptureSessionInterruptionReason. Add observers to
+        // handle these session interruptions and show a preview is paused
+        // message. See `AVCaptureSessionWasInterruptedNotification` for other
+        // interruption reasons.
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(sessionWasInterrupted),
+                                               name: .AVCaptureSessionWasInterrupted,
+                                               object: session)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(sessionInterruptionEnded),
+                                               name: .AVCaptureSessionInterruptionEnded,
+                                               object: session)
     }
+
+    @objc func subjectAreaDidChange(notification: NSNotification) {
+        let devicePoint = CGPoint(x: 0.5, y: 0.5)
+        focus(with: .continuousAutoFocus, exposureMode: .continuousAutoExposure, at: devicePoint, monitorSubjectAreaChange: false)
+    }
+
+    @objc func sessionRuntimeError(notification: NSNotification) {
+        guard let error = notification.userInfo?[AVCaptureSessionErrorKey] as? AVError else { return }
+
+        print("Capture session runtime error: \(error)")
+        // If media services were reset, and the last start succeeded, restart the session
+        if error.code == .mediaServicesWereReset {
+            sessionQueue.async {
+                if self.isSessionRunning {
+                    // Line below restarts the session
+                    self.session.startRunning()
+                    self.isSessionRunning = self.session.isRunning
+                } else {
+                    DispatchQueue.main.async {
+                        self.resumeButton.isHidden = false
+                    }
+                }
+            }
+        } else {
+            resumeButton.isHidden = false
+        }
+    }
+
+    @objc func sessionWasInterrupted(notification: NSNotification) {
+        // In some scenarios you want to enable the user to resume the session.
+        // For example, if music playback is initiated from Control Center while
+        // using AVCam, then the user can let AVCam resume the session running,
+        // which will stop music playback. Note that stopping music playback in
+        // Control Center will not automatically resume the session. Also note
+        // that it's not always possible to resume, see
+        // `resumeInterruptedSession(_:)`.
+        if let userInfoValue = notification.userInfo?[AVCaptureSessionInterruptionReasonKey] as AnyObject?,
+           let reasonIntegerValue = userInfoValue.integerValue,
+           let reason = AVCaptureSession.InterruptionReason(rawValue: reasonIntegerValue) {
+            print("Capture session was interrupted with reason \(reason)")
+
+            var showResumeButton = false
+            if reason == .audioDeviceInUseByAnotherClient || reason == .videoDeviceInUseByAnotherClient {
+                showResumeButton = true
+            } else if reason == .videoDeviceNotAvailableWithMultipleForegroundApps {
+                // Fade-in a label to inform the user that the camera is
+                // unavailable.
+                cameraUnavailableLabel.alpha = 0
+                cameraUnavailableLabel.isHidden = false
+                UIView.animate(withDuration: 0.25) {
+                    self.cameraUnavailableLabel.alpha = 1
+                }
+            } else if reason == .videoDeviceNotAvailableDueToSystemPressure {
+                print("Session stopped running due to shutdown system pressure level.")
+            }
+            if showResumeButton {
+                // Fade-in a button to enable the user to try to resume the
+                // session running.
+                resumeButton.alpha = 0
+                resumeButton.isHidden = false
+                UIView.animate(withDuration: 0.25) {
+                    self.resumeButton.alpha = 1
+                }
+            }
+        }
+    }
+
+    @objc func sessionInterruptionEnded(notification: NSNotification) {
+        print("Capture session interruption ended")
+
+        if !resumeButton.isHidden {
+            UIView.animate(withDuration: 0.25,
+                           animations: {
+                            self.resumeButton.alpha = 0
+            }, completion: { _ in
+                self.resumeButton.isHidden = true
+            })
+        }
+        if !cameraUnavailableLabel.isHidden {
+            UIView.animate(withDuration: 0.25,
+                           animations: {
+                            self.cameraUnavailableLabel.alpha = 0
+            }, completion: { _ in
+                self.cameraUnavailableLabel.isHidden = true
+            }
+            )
+        }
+    }
+
+    private func removeObservers() {
+        NotificationCenter.default.removeObserver(self)
+
+        for keyValueObservation in keyValueObservations {
+            keyValueObservation.invalidate()
+        }
+        keyValueObservations.removeAll()
+    }
+
+    // TODO: Come back to this when implimenting switching cameras
+//    private var systemPreferredCameraContext = 0
+//
+//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+//        if context == &systemPreferredCameraContext {
+//            guard let systemPreferredCamera = change?[.newKey] as? AVCaptureDevice else { return }
+//
+//            // Don't switch cameras if movie recording is in progress.
+//            if let movieFileOutput = self.movieFileOutput, movieFileOutput.isRecording {
+//                return
+//            }
+//            if self.videoDeviceInput.device == systemPreferredCamera {
+//                return
+//            }
+//
+//            self.changeCamera(systemPreferredCamera, isUserSelection: false)
+//        } else {
+//            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+//        }
+//    }
 }
 
 
